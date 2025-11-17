f881cc59ed1909a2eacde6bb52b42ca07f4d2260
Francisca Cerqueira <162129943+francisca005@users.noreply.github.com>
2025-11-17 14:23:07 +0000

    Refactor imports and code formatting in TabGame

--- js/TabGame.js
+++ js/TabGame.js
@@ -1,5 +1,5 @@
 // js/TabGame.js
-import { Piece } from "./piece.js";
+import { Piece } from "./Piece.js";
 import { AIPlayer } from "./AIPlayer.js";
 
 // Classe Principal — TabGame
@@ -570,6 +570,7 @@ export class TabGame {
 
 
     }
+
 
 
 }
